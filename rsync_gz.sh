6b320f42764bad29e7eeee71df2ff9d659ee242a
Zhang Qiang <qiangzhang@qiyi.com>
2016-06-05 23:16:35 +0800

    rsync with parallel mode

--- rsync_gz.sh
+++ rsync_gz.sh
@@ -1,11 +1,28 @@
 #!/bin/env bash
 
 NF=4800
+N_POOL=300
+
+function process_pool_stall ()
+{
+    while :; do
+        local n_proc=`jobs -r | wc -l`
+        if [ ${n_proc} -lt ${N_POOL} ]; then
+            break
+        fi
+        echo "sleep"
+        sleep 1
+    done
+}
+
+
 for ((i=0; i< $NF; i++))
 do
-    rm -fr /mm/$i.gz
+    process_pool_stall
+    rm -f /mm/$i.gz &
 done
 
+echo "Start rsync ..."
 time rsync -avr /mm1/ /mm >/dev/null 2>&1
 
 echo "done"
